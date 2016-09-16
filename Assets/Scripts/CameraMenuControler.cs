ba33fa4081c94cfb02aacc115083ab331e7a04c8
Maarti <bryan.martinet@gmail.com>
2016-09-17 01:57:29 +0200

    test new branch

---
+++ Assets/Scripts/CameraMenuControler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;   // necessaire pour gerer les element de l'UI
+
+public class CameraMenuControler : MonoBehaviour {
+
+    public float minFoV = 12f;
+    public float maxFoV = 60f;
+    public float sensitivity = 10f;
+    public CanvasGroup scrollView;
+
+    void Start()
+    {
+        scrollView.alpha = 0f;
+        StartCoroutine(ZoomInCoroutine());
+    }
+
+    IEnumerator ZoomInCoroutine()
+    {
+        while (Camera.main.fieldOfView > minFoV)
+        {
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView-1, minFoV, maxFoV);
+            yield return new WaitForSeconds(1/sensitivity);
+        }
+        while (scrollView.alpha < 1f)
+        {
+            scrollView.alpha += .1f;
+            yield return new WaitForSeconds(.1f);
+        }            
+    }
+}
---
+++ Assets/Scripts/GenerateLevelButtons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;   // necessaire pour gerer les element de l'UI
+
+public class GenerateLevelButtons : MonoBehaviour {
+
+    public Button buttonPrefab;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 2; i < 20; i++)
+        {
+            Button button = Instantiate(buttonPrefab, gameObject.transform) as Button;
+            button.transform.SetParent(gameObject.transform, false);
+            button.transform.localScale = Vector3.one;
+            button.GetComponentInChildren<Text>().text = "Project " + i;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
