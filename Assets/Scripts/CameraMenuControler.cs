9342c9fed8d930663d1f672442b93ef633e7aa6e
Maarti <bryan.martinet@gmail.com>
2016-09-18 19:21:59 +0200

    init shop menu and howToPlay menu

--- Assets/Scripts/CameraMenuControler.cs
+++ Assets/Scripts/CameraMenuControler.cs
@@ -9,14 +9,14 @@ public class CameraMenuControler : MonoBehaviour
 
 	public float minFoV = 1f, screenFov = 29f, maxFoV = 60f;
 	public float sensitivity = 10f;
-	public CanvasGroup uiTouchScreen, uiMainMenu, uiProjectScroller;
+	public CanvasGroup uiTouchScreen, uiMainMenu, uiPlayMenu, uiHowToPlayMenu, uiShopMenu;
 	public GameObject computer;
 
 	// "float" 			= floating around;
-	// "menustart" 		= zooming on screen;
-	// "menuend" 		= zoomed on screen+menu;
-	// "projectstart"	= zooming in blackscreen;
-	// "projectend"		= zoomed in blackscreen;
+	// "mainmenustart" 		= zooming on screen;
+	// "mainmenuend" 		= zoomed on screen+menu;
+	// "menustart"	= zooming in blackscreen;
+	// "menuend"		= zoomed in blackscreen;
 	string state = "float";
 	float cameraZInit;
 	Coroutine fadeInUiTouchScreen, fadeInUiMainMenu, fadeInUiProjectScroller;
@@ -30,7 +30,9 @@ public class CameraMenuControler : MonoBehaviour
 
 		HideUI (uiTouchScreen);
 		HideUI (uiMainMenu);
-		HideUI (uiProjectScroller);
+		HideUI (uiPlayMenu);
+		HideUI (uiHowToPlayMenu);
+		HideUI (uiShopMenu);
 
 		StartCoroutine (FloatingCoroutine ());
 		fadeInUiTouchScreen = StartCoroutine (FadeInUICoroutine (uiTouchScreen));
@@ -39,17 +41,12 @@ public class CameraMenuControler : MonoBehaviour
 	void Update ()
 	{
 		if (state == "float" && ((Input.touchCount > 0) || (SystemInfo.deviceType == DeviceType.Desktop && Input.GetButton ("Fire1")))) {
-			state = "menustart";
+			state = "mainmenustart";
 			StopCoroutine (FloatingCoroutine ());
 			StopCoroutine (fadeInUiTouchScreen);
 			HideUI (uiTouchScreen);
 			StartCoroutine (MoveCameraToStartCoroutine ());
-			StartCoroutine (ZoomInCoroutine ());
-		} else if (state == "menuend" && ((Input.touchCount > 0) || (SystemInfo.deviceType == DeviceType.Desktop && Input.GetButton ("Fire1")))) {
-			state = "projectstart";
-			StopCoroutine (fadeInUiMainMenu);
-			HideUI (uiMainMenu);
-			StartCoroutine (ZoomInCoroutine ());
+			StartCoroutine (ZoomInCoroutine (uiMainMenu));
 		}
 	}
 
@@ -73,26 +70,37 @@ public class CameraMenuControler : MonoBehaviour
 		}
 	}
 
-	IEnumerator ZoomInCoroutine ()
+	IEnumerator ZoomInCoroutine (CanvasGroup uiToDisplay)
 	{
-		if (state == "menustart") {
+		if (state == "mainmenustart") {
 			while (Camera.main.fieldOfView > screenFov) {
 				Camera.main.transform.LookAt (computer.transform);
-				Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView - 1, screenFov, maxFoV);
-				yield return new WaitForSeconds (1 / sensitivity);
+				Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView - .5f, screenFov, maxFoV);
+				yield return new WaitForSeconds (0.025f);
 			}
-			fadeInUiMainMenu = StartCoroutine (FadeInUICoroutine (uiMainMenu));
-			state = "menuend";
+			fadeInUiMainMenu = StartCoroutine (FadeInUICoroutine (uiToDisplay));
+			state = "mainmenuend";
 
-		} else if (state == "projectstart") {			
+		} else if (state == "menustart") {			
 			while (Camera.main.fieldOfView > minFoV) {
 				Camera.main.transform.LookAt (computer.transform);
-				Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView - 1, minFoV, maxFoV);
+				Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView - 1f, minFoV, screenFov);
 				yield return new WaitForSeconds (1 / sensitivity);
 			}
-			fadeInUiProjectScroller = StartCoroutine (FadeInUICoroutine (uiProjectScroller));
-			state = "projectend";
+			fadeInUiProjectScroller = StartCoroutine (FadeInUICoroutine (uiToDisplay));
+			state = "menuend";
 		}
+	}
+
+	IEnumerator ZoomOutCoroutine ()
+	{
+		while (Camera.main.fieldOfView < screenFov) {
+			Camera.main.transform.LookAt (computer.transform);
+			Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView + 1f, minFoV, screenFov);
+			yield return new WaitForSeconds (1 / sensitivity);
+		}
+		fadeInUiMainMenu = StartCoroutine (FadeInUICoroutine (uiMainMenu));
+		state = "mainmenuend";
 	}
 
 	IEnumerator MoveCameraToStartCoroutine ()
@@ -111,8 +119,8 @@ public class CameraMenuControler : MonoBehaviour
 		}				
 		while (Camera.main.transform.position.z != cameraZInit) {
 			Camera.main.transform.LookAt (computer.transform);
-			Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, Mathf.Clamp (Camera.main.transform.position.z + 0.1f * multiplier, min, max));
-			yield return new WaitForSeconds (.05f);
+			Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, Mathf.Clamp (Camera.main.transform.position.z + 0.02f * multiplier, min, max));
+			yield return new WaitForSeconds (.025f);
 		}
 	}
 
@@ -125,11 +133,47 @@ public class CameraMenuControler : MonoBehaviour
 	IEnumerator FadeInUICoroutine (CanvasGroup ui)
 	{
 		// On fait apparaitre le menu
+		ui.blocksRaycasts = true;
 		while (ui.alpha < 1f) {
 			ui.alpha += .05f;
 			yield return new WaitForSeconds (.1f);
 		}
-		ui.blocksRaycasts = true;
+	}
+
+	public void backToMenu ()
+	{
+		state = "mainmenustart";
+		StopAllCoroutines ();
+		HideUI (uiTouchScreen);
+		HideUI (uiMainMenu);
+		HideUI (uiPlayMenu);
+		HideUI (uiHowToPlayMenu);
+		HideUI (uiShopMenu);
+		StartCoroutine (ZoomOutCoroutine ());
+	}
+
+	public void OnClickPlay ()
+	{
+		state = "menustart";
+		StopCoroutine (fadeInUiMainMenu);
+		HideUI (uiMainMenu);
+		StartCoroutine (ZoomInCoroutine (uiPlayMenu));
+	}
+
+	public void OnClickHowToPlay ()
+	{
+		state = "menustart";
+		StopCoroutine (fadeInUiMainMenu);
+		HideUI (uiMainMenu);
+		StartCoroutine (ZoomInCoroutine (uiHowToPlayMenu));
+	}
+
+	public void OnClickShop ()
+	{
+		state = "menustart";
+		StopCoroutine (fadeInUiMainMenu);
+		HideUI (uiMainMenu);
+		StartCoroutine (ZoomInCoroutine (uiShopMenu));
 	}
 
 		
